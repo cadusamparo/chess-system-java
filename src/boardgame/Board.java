eff7bb1ccd2c80cfdcab804c1abfa4605a783cc3
Carlos Eduardo Braz Samparo <carlossamparoo@gmail.com>
2023-03-31 10:32:17 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -25,7 +25,12 @@ public class Board {
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
+    }
 
-
+    public Piece piece(int row , int columns){
+        return pieces[row][columns];
+    }
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
     }
 }
